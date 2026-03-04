59db98b15ce1c21e0464e14ebe5897218cc3ab8c
M4thys02 <matej.marousek@seznam.cz>
2026-03-04 22:19:48 +0100

    separate menu logic to individual js file

--- js/main.js
+++ js/main.js
@@ -1,24 +1,5 @@
-const burger = document.querySelector(".burger");
-const menu = document.getElementById("menu");
-const overlay = document.getElementById("overlay");
 const searchInput = document.getElementById('recipe-search');
 const resultsList = document.getElementById('search-results');
-
-burger.addEventListener("click", () => {
-    burger.classList.toggle("active");
-    menu.classList.toggle("open");
-    overlay.classList.toggle("show");
-
-    const expanded = burger.getAttribute("aria-expanded") === "true";
-    burger.setAttribute("aria-expanded", !expanded);
-});
-
-overlay.addEventListener("click", () => {
-    burger.classList.remove("active");
-    menu.classList.remove("open");
-    overlay.classList.remove("show");
-    burger.setAttribute("aria-expanded", "false");
-});
 
 if (searchInput) {
     searchInput.addEventListener('input', async (e) => {
